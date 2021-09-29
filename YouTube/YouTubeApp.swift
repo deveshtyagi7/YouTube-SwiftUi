ee231f8a2907ae1d0042b2a5e7e7839b3c2e0540
Devesh Tyagi <tyagi77@outlook.com>
2021-09-29 13:35:51 +0530

    Initial Commit

---
+++ YouTube/YouTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTubeApp.swift
+//  YouTube
+//
+//  Created by Devesh Tyagi on 29/09/21.
+//
+
+import SwiftUI
+
+@main
+struct YouTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
